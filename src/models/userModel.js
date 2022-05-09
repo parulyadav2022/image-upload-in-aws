c2910e9ff14655f88c6ce422c9df53419610a67f
Darshit-dev <khenidarshit@gmail.com>
2022-05-09 21:16:24 +0530

    added trim in chema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,10 +2,10 @@ const mongoose  = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     title : {type : String, required : true,enum : ["Mr","Mrs","Miss"]},
-    name : {type :String,required : true},
+    name : {type :String,required : true,trim:true},
     phone :{type : String, required : true,unique : true},
-    email : {type :String, required : true,unique : true},
-    password : {type : String, required : true,min:8,max :15},
+    email : {type :String, required : true,unique : true,trim:true},
+    password : {type : String, required : true,min:8,max :15,trim:true},
     address : {street:String,city:String,pincode :String}
     
         
