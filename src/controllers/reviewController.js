2c9cc84e195b472890c0770a1678b5011d09cf77
Darshit-dev <khenidarshit@gmail.com>
2022-05-11 14:04:22 +0530

    review 1st done

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,46 +1,80 @@
-const reviewModel = require('../models/reviewModel');
-const bookModel = require('../models/bookModel');
-const mongoose = require('mongoose');
+const reviewModel = require("../models/reviewModel");
+const bookModel = require("../models/bookModel");
+const mongoose = require("mongoose");
 
-const isValid = function(value){
-    if(typeof value === 'undefined' || value === null )return false;
-    if(typeof value === 'string' && value.trim().length === 0)return false;
-    return true;
-}
+const isValid = function (value) {
+  if (typeof value === "undefined" || value === null) return false;
+  if (typeof value === "string" && value.trim().length === 0) return false;
+  return true;
+};
 
-
-const isvalidObjectId = function(objectId) {
-    return mongoose.Types.ObjectId.isValid(objectId);
-}
+const isvalidObjectId = function (objectId) {
+  return mongoose.Types.ObjectId.isValid(objectId);
+};
 
 //POST /books/:bookId/review
-const addReview = async function(req,res){
-    try{
-      const {bookId} = req.params
-      const  getBodyData = req.body
-      req.body.bookId = bookId
-//validation
-//if(isvalidObjectId(bookId)){return res.status(403).send({status: false},{message:'Invalid bookId'})}
- const isBookAvalilable  = await bookModel.findOne({_id:bookId,isDeleted: false}).lean()
- if(!isBookAvalilable){return res.status(404).send({message:'Book is not available'})}
- 
+const addReview = async function (req, res) {
+  try {
+    const { bookId } = req.params;
+    const getBodyData = req.body;
+    req.body.bookId = bookId;
+    //validation
+    //if(isvalidObjectId(bookId)){return res.status(403).send({status: false},{message:'Invalid bookId'})}
+    const isBookAvalilable = await bookModel
+      .findOne({ _id: bookId, isDeleted: false })
+      .lean();
+    if (!isBookAvalilable) {
+      return res.status(404).send({ message: "Book is not available" });
+    }
 
-      const {reviewedBy,rating} = getBodyData
-      if(!Number.isInteger(rating)){return res.status(400).send({status: false,message : "rating should be in number"})}
-      if(rating<1 && rating>5){return res.status(400).send({status: false,message:"rating must be in the range of 1-5"})}
-      getBodyData.reviewedAt = Date.now() 
+    const { rating } = getBodyData;
+
+    //if user enters any any non integer value
+    if (!Number.isInteger(rating)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "rating should be in number" });
+    }
+
+    //wrong validation
+    if (rating < 1 || rating > 5) {
+      return res
+        .status(400)
+        .send({ status: false, message: "rating must be in the range of 1-5" });
+    }
+
+    //if passing validation
+
+    //reviewdAt key added
+    getBodyData.reviewedAt = Date.now();
+
+    //create entry in review model
+    await reviewModel.create(getBodyData);
+
+    //to increment reiew in bookmodel after adding review
+    const Updatedbooks = await bookModel
+      .findOneAndUpdate(
+        { _id: bookId },
+        { $inc: { reviews: 1 } },
+        { new: true }
+      )
+      .lean();
+
+      //!new changes added to get data review data in array
+    const getReviewsData = await reviewModel.find({bookId:bookId})
+    console.log(getReviewsData);
+    //added createreview data into reviewsData key
+    Updatedbooks.reviewsData = getReviewsData;
+
+    return res.status(200).send({
+      status: true,
+      message: "Successfully added review",
+      data: Updatedbooks,
+    });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message });
+  }
+};
 
 
-      const createReview = await reviewModel.create(getBodyData)
-
-      const Updatedbooks =await bookModel.findOneAndUpdate({_id:bookId},{$inc : {reviews : 1}},{new:true}).lean()
-    
-     Updatedbooks.reviewsData =createReview 
-     
-    return res.status(200).send({status: true,message : "Successfully added review",data:Updatedbooks})
-
-    }catch(error){
-        return res.status(500).send({status: false,message : error.message})
-    }   
-}
-module.exports = {addReview};
+module.exports = { addReview };
