33308f4389a0a4d3a00f37c5e8fb2d2b047a6c54
Darshit-dev <khenidarshit@gmail.com>
2022-05-11 13:13:42 +0530

    1st review api added

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -15,16 +15,32 @@ const isvalidObjectId = function(objectId) {
 
 //POST /books/:bookId/review
 const addReview = async function(req,res){
+    try{
       const {bookId} = req.params
       const  getBodyData = req.body
-        
-      //validation
-      if(isvalidObjectId(bookId)){return res.status(403).render('error',{message:'Invalid bookId'})}
+      req.body.bookId = bookId
+//validation
+//if(isvalidObjectId(bookId)){return res.status(403).send({status: false},{message:'Invalid bookId'})}
+ const isBookAvalilable  = await bookModel.findOne({_id:bookId,isDeleted: false}).lean()
+ if(!isBookAvalilable){return res.status(404).send({message:'Book is not available'})}
+ 
 
-      //logic
+      const {reviewedBy,rating} = getBodyData
+      if(!Number.isInteger(rating)){return res.status(400).send({status: false,message : "rating should be in number"})}
+      if(rating<1 && rating>5){return res.status(400).send({status: false,message:"rating must be in the range of 1-5"})}
+      getBodyData.reviewedAt = Date.now() 
 
-      const review = await reviewModel.create()
 
+      const createReview = await reviewModel.create(getBodyData)
+
+      const Updatedbooks =await bookModel.findOneAndUpdate({_id:bookId},{$inc : {reviews : 1}},{new:true}).lean()
     
+     Updatedbooks.reviewsData =createReview 
+     
+    return res.status(200).send({status: true,message : "Successfully added review",data:Updatedbooks})
+
+    }catch(error){
+        return res.status(500).send({status: false,message : error.message})
+    }   
 }
 module.exports = {addReview};
